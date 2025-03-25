3ea07d844a70a60695af49b57bac676d767b4666
Sofiazpiroz <sofiazpiroz2004@gmail.com>
2025-03-25 15:58:35 +0100

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,4 +1,4 @@
-import simpy
+mport simpy
 from process import generar_incidentes
 from entities import Ambulancia, CamionBomberos, PatrullaPolicia
 
@@ -8,14 +8,11 @@ if __name__ == "__main__":
     env = simpy.Environment()
 
     # Definimos los recursos necesarios para atender las emergencias
-    recursos = [
-        Ambulancia, CamionBomberos, PatrullaPolicia
-    ]
+    recursos = [ Ambulancia, CamionBomberos, PatrullaPolicia]
 
     # Generamos los incidentes
     env.process(generar_incidentes(env, [], recursos))
 
     # Ejecutamos la simulación por 30 unidades de tiempo
     env.run(until=30)
-
     
