d2e04498b1544bf8a49e9f5f2d020eb2f601193f
Sofiazpiroz <sofiazpiroz2004@gmail.com>
2025-03-25 15:56:16 +0100

    Update entities.py

--- entities.py
+++ entities.py
@@ -1,5 +1,5 @@
 
-import random
+iimport random
 
 '''Clases'''
 
@@ -17,7 +17,7 @@ class Incidente:
         return f"Incidente {self.id_incidente}: {self.tipo} en {self.ubicacion} (Prioridad {self.prioridad})"
 
 
-class Recurso:
+class Recurso(Incidente):
     def __init__(self, env, tipo, ubicacion):
         self.env = env
         self.tipo = tipo
@@ -27,16 +27,16 @@ class Recurso:
     def asignar(self, incidente):
         if self.disponible:
             self.disponible = False
-            print(f"{self.tipo} asignado a {incidente} en el tiempo {self.env.now}")
+            print(f"{self.tipo} asignado a {incidente} en {self.env.now} minutos")
             yield self.env.timeout(incidente.tiempo_resolucion)  # Tiempo de atención al cliente
             self.disponible = True
-            print(f"{self.tipo} disponible nuevamente en el tiempo {self.env.now}")
+            print(f"{self.tipo} disponible nuevamente en {self.env.now} minutos")
 
     def __str__(self):
         return f"{self.tipo} en {self.ubicacion}"
 
 
-# Subclases de "Recurso"
+# Subclases de la clase "Recurso"
 class Ambulancia(Recurso):
     def __init__(self, env, ubicacion):
         super().__init__(env, "Ambulancia", ubicacion)
@@ -48,7 +48,6 @@ class CamionBomberos(Recurso):
 class PatrullaPolicia(Recurso):
     def __init__(self, env, ubicacion):
         super().__init__(env, "Patrulla de Policía", ubicacion)
-        super()._init_(env, "Camión de Bomberos", ubicacion)
 
 class PatrullaPolicia(Recurso):
     def _init_(self, env, ubicacion):
