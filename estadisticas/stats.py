c4c79195c6d1fd92653be3284baa4130ca4a2d61
eduardobernad33 <eduardo.bernad@cunef.edu>
2025-04-04 15:54:25 +0200

    stats.py

--- estadisticas/stats.py
+++ estadisticas/stats.py
@@ -1,13 +1,16 @@
+# Importamos las librerias necesarias como pandas para manejar estructuras de datos y os para trabajar con rutas de archivos y carpetas
 import pandas as pd
 import os #Para meter el archivo donde yo quiero
 
+# La clase estadisticas se encarga de almacenar y guardar informacion de incidentes
 class Estadisticas:
     def __init__(self):
+        #Inicializamos la lista vacia donde se iran almacenando los accidentes
         self.datos = []
         self.directorio = "estadisticas"  
         self.archivo_csv = os.path.join(self.directorio, "incidentes.csv")  
 
-
+# Este metodo lo utilzamos para agregar informacion de un nuevo incidente
     def agregar_datos(self, id_incidente: int, tipo: str, prioridad: int, 
                       ubicacion: str, tiempo_espera: float, tiempo_resolucion: float, 
                       recurso_asignado: str) -> None:
@@ -23,6 +26,7 @@ class Estadisticas:
             tiempo_resolucion (float): Tiempo desde la asignación hasta la resolución.
             recurso_asignado (str): Recurso asignado al incidente.
         """
+    # Diccionario con la informacion de los incidentes 
         datos_incidente = {
             'ID Incidente': id_incidente,
             'Tipo': tipo,
@@ -34,11 +38,11 @@ class Estadisticas:
         }
         
         self.datos.append(datos_incidente)
-
+# Convierte la lista de datos en un DF e pandas
     def a_dataframe(self):
         """Convierte los datos en un DataFrame de Pandas."""
         return pd.DataFrame(self.datos)
-
+# Por ultimo utilizamos guardar_csv para guardar los datos en un archivo CSV
     def guardar_csv(self, nombre_archivo='estadisticas/estadisticas_incidentes.csv'):
         """Guarda los datos en un archivo CSV."""
         df = self.a_dataframe()
