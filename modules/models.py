85aae19bf05b25e3af089bc1afbee62494277595
Sofiazpiroz <sofiazpiroz2004@gmail.com>
2025-04-04 17:46:35 +0200

    Update models.py

--- modules/models.py
+++ modules/models.py
@@ -1,13 +1,16 @@
+# Clase que representa un incidente dentro del sistema
 class Incidente:
     def __init__(self, id, tipo, prioridad, ubicacion):
-        self.id = id
-        self.tipo = tipo
-        self.prioridad = prioridad
-        self.ubicacion = ubicacion
-        self.tiempo_creacion = None
+        self.id = id # Identificador del incidente
+        self.tipo = tipo # Tipo de incidente = "Robo", "Incendio", "Accidente"
+        self.prioridad = prioridad # Nivel de prioridad del incidente (1 mas alto)
+        self.ubicacion = ubicacion # Distrito donde ocurre el incidente
+        self.tiempo_creacion = None # Tiempo simulado en el que se genera el incidente
 
+
+# Clase que representa un recurso de emergencia
 class Recurso:
     def __init__(self, id, tipo):
-        self.id = id
-        self.tipo = tipo
-        self.disponible = True
+        self.id = id # Identificador del recurso
+        self.tipo = tipo  # Tipo de recurso = "Ambulancia", "Camión de Bomberos", "Policía"
+        self.disponible = True # Indica si el recurso esta disponible u ocupado
