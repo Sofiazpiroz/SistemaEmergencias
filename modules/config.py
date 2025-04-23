f6b600eaa211bb06cad16f248682940bd2b6efb6
Sofiazpiroz <sofiazpiroz2004@gmail.com>
2025-04-23 16:42:26 +0200

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,13 +1,13 @@
 # Importamos la clase Recurso desde el modulo models
 from modules.models import Recurso
 
-# Lista de recursos disponibles en el sistema
-# Cada recurso tiene un ID y un tipo (nombre del vehiculo de emergencia)
-recursos = [
-    Recurso(1, "Ambulancia"), # Recurso para atender accidentes
-    Recurso(2, "Camion de Bomberos"), # Recurso para atender incendios
-    Recurso(3, "Patrulla de Policia") # Recurso para atender robos
-]
+# Lista de ubicaciones posibles donde pueden ocurrir incidentes
+# Corresponde a los 21 distritos de la ciudad de Madrid
+ubicaciones = ["Centro", "Arganzuela", "Retiro", "Salamanca", "Chamartín", "Tetuán", 
+                "Chamberí", "Fuencarral-El Pardo", "Moncloa", "Latina", "Carabanchel",
+                "Usera", "Puente de Vallecas", "Moratalaz", "Ciudad Lineal", "Hortaleza",
+                "Villaverde", "Villa de Vallecas", "Vicálvaro", "San Blas", "Barajas" ]
+
 
 # Diccionario que asocia cada tipo de incidente con el recurso necesario para atenderlo
 tipo_recurso = {
@@ -16,14 +16,21 @@ tipo_recurso = {
     "Robo": "Patrulla de Policia"
 }
 
-# Lista de ubicaciones posibles donde pueden ocurrir incidentes
-# Corresponde a los 21 distritos de la ciudad de Madrid
-ubicaciones = ["Centro", "Arganzuela", "Retiro", "Salamanca", "Chamartín", "Tetuán", 
-                "Chamberí", "Fuencarral-El Pardo", "Moncloa", "Latina", "Carabanchel",
-                "Usera", "Puente de Vallecas", "Moratalaz", "Ciudad Lineal", "Hortaleza",
-                "Villaverde", "Villa de Vallecas", "Vicálvaro", "San Blas", "Barajas" ]
+
+# Lista de recursos disponibles en el sistema
+# Cada recurso tiene un ID y un tipo (nombre del vehiculo de emergencia)
+recursos = []
+
+id_counter = 1
+for ubicacion in ubicaciones:  # ["Distrito 1", "Distrito 2", ...]
+    for tipo in tipo_recurso.values():  # ["Bombero", "Ambulancia", "Policía"]
+        for _ in range(2):  # Número de recursos por tipo y ubicación
+            recursos.append(Recurso(id=id_counter, tipo=tipo, ubicacion=ubicacion))
+            id_counter += 1
+
 
 # Cola de incidentes pendientes por atender
 # Aqui se almacenarán temporalmente los incidenes que no puedan ser atendidos al momento
 cola_incidentes = []
+
 
